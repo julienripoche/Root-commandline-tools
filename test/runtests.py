9ead4cfb862f27f4608f69f669400ec076676311
Julien Ripoche <jripoche@pcphsft82.cern.ch>
2015-06-26 11:44:58 +0200

    Adding some other test on rools, changing of ROOT file for test

--- test/runtests.py
+++ test/runtests.py
@@ -8,11 +8,17 @@ def testCommand(testName,command,refFileName):
     command += " >& %s" %ofileName
     os.system(command)
     print "Test %s" %testName,
-    if not filecmp.cmp(ofileName,refFileName):
-        print "FAILURE"
-    else:
+    if filecmp.cmp(ofileName,refFileName):
         print "SUCCESS"
-    
+    else:
+        print "FAILURE"
 
-testCommand("SimpleRools", "rools hsimple.root", "simpleLs.ref")
-testCommand("SimpleRools2", "rools hsimple.root:*", "simpleLs2.ref")
+testCommand("SimpleRools", "rools test.root", "SimpleRools.ref")
+testCommand("SimpleRools2", "rools test.root:*", "SimpleRools2.ref")
+testCommand("SimpleRools3", "rools test.root:tof", "SimpleRools3.ref")
+testCommand("SimpleRools4", "rools test.root:tof/*", "SimpleRools4.ref")
+
+testCommand("LongRools", "rools -l test.root", "LongRools.ref")
+testCommand("LongRools2", "rools -l test.root:*", "LongRools2.ref")
+testCommand("LongRools3", "rools -l test.root:tof", "LongRools3.ref")
+testCommand("LongRools4", "rools -l test.root:tof/*", "LongRools4.ref")
