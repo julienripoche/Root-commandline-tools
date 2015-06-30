9d59e9ee493195e8e07659636dab0a2aeb0dd006
Julien Ripoche <jripoche@pcphsft82.cern.ch>
2015-06-30 15:34:02 +0200

    Removing of ansi characters if the output is not the terminal
        o Bold function return bold string if sys.stdout.isatty()
        o Change the corresponding tests

--- python/pretty_print.py
+++ python/pretty_print.py
@@ -2,14 +2,22 @@
 
 """Module which contains some pretty print functions"""
 
+import sys
+
 def ansi_bold(string):
     """Make the string bold"""
-    ansi_bold = "\x1B[1m"
-    ansi_end = "\x1B[0m"
-    return ansi_bold+string+ansi_end
+    if sys.stdout.isatty():
+        ansi_bold = "\x1B[1m"
+        ansi_end = "\x1B[0m"
+        return ansi_bold+string+ansi_end
+    else:
+        return string
 
 def ansi_blue(string):
     """Make the string blue"""
-    ansi_blue = "\x1B[34m"
-    ansi_end = "\x1B[0m"
-    return ansi_blue+string+ansi_end
+    if sys.stdout.isatty():
+        ansi_blue = "\x1B[34m"
+        ansi_end = "\x1B[0m"
+        return ansi_blue+string+ansi_end
+    else:
+        return string
