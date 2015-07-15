94a7e8b90e14a7bfd610e03d3a528bb2fdf76fd5
Julien Ripoche <jripoche@pcphsft82.cern.ch>
2015-07-15 17:17:41 +0200

    Merging of pprint_long_ls and pprint_tree_ls functionalities

--- python/rools_print.py
+++ python/rools_print.py
@@ -4,7 +4,6 @@
 with a rools style"""
 
 from rools_utils.pprint_long_ls import *
-from rools_utils.pprint_tree_ls import *
 from rools_utils.pprint_ls import *
 import ROOT
 
@@ -12,9 +11,7 @@ def rools_print(key_list,opt_dict):
     """Print the informations given by key_list with a rools
     style choosen with opt_dict"""
     if opt_dict['l']:
-        pprint_long_ls(key_list)
-    elif opt_dict['t']:
-        pprint_tree_ls(key_list)
+        pprint_long_ls(key_list,opt_dict)
     else:
         # Don't forget color and bold, make a different function
         name_list = [key.GetName() for key in key_list]
--- python/rools_utils/pprint_long_ls.py
+++ python/rools_utils/pprint_long_ls.py
@@ -4,6 +4,42 @@
 
 import ROOT
 from pretty_print import *
+from utils import *
+
+# Templates for columns print
+template = ansi_bold("{0:{class_width}}")+"{1:{time_width}}{2:{name_width}}{3:{title_width}}"
+tree_template = "{0:{name_width}}"+"{1:{title_width}}{2:{memory_width}}"
+
+def recursif(tree,indentLevel):
+    # Width informations
+    if len(tree.GetListOfBranches()) > 0:
+        max_name = max([len(branch.GetName()) for branch in tree.GetListOfBranches()])
+        max_title = max([len(branch.GetTitle()) for branch in tree.GetListOfBranches()])
+        #max_memory = max([len(str(branch.GetTotBytes())) for branch in tree.GetListOfBranches()])
+        dic = {"name_width":max_name+2,"title_width":max_title+4,"memory_width":1}
+
+    # Print loop
+    for branch in tree.GetListOfBranches():
+        # Data
+        rec = [branch.GetName(), \
+               "\""+branch.GetTitle()+"\"", \
+               str(branch.GetTotBytes())]
+         # Print
+        print "  "*indentLevel + tree_template.format(*rec,**dic)
+        # if indentLevel < opt_dict["deeper_level"]: to keep in mind
+        recursif(branch,indentLevel+1)
+
+#def pprint_tree_ls(key_list):
+#    """Function to print contents of trees"""
+#    for key in key_list:
+#        if is_tree(key):
+#            tree = key.ReadObj()
+#            rec = [tree.GetName(), \
+#               "\""+tree.GetTitle()+"\"", \
+#               str(tree.GetTotBytes())]
+#            dic = {"name_width":len(tree.GetName())+2,"title_width":len(tree.GetTitle())+4,"memory_width":1}
+#            print tree_template.format(*rec,**dic)
+#            recursif(tree,1)
 
 def time_preparator(time):
     """ Function to be sure of the format of time
@@ -13,12 +49,9 @@ def time_preparator(time):
     time = time[len(time)-6:]
     return time
 
-def pprint_long_ls(key_list):
+def pprint_long_ls(key_list,opt_dict):
     """Function to print a list of Tkey in columns,
     classname, datetime, name and title"""
-
-    # Template for columns print
-    template = ansi_bold("{0:{class_width}}")+"{1:{time_width}}{2:{name_width}}{3:{title_width}}"
 
     # Width informations
     if len(key_list) > 0:
@@ -39,3 +72,6 @@ def pprint_long_ls(key_list):
                key.GetName(), \
                "\""+key.GetTitle()+"\""]
         print template.format(*rec,**dic)
+        if opt_dict['t'] and is_tree(key):
+            tree = key.ReadObj()
+            recursif(tree,1)
