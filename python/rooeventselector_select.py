f33296f83ee6d365c792f5a538cd40493239d18b
Julien Ripoche <jripoche@pcphsft82.cern.ch>
2015-07-16 17:51:57 +0200

    Some improvements for rooevenselector

--- python/rooeventselector_select.py
+++ python/rooeventselector_select.py
@@ -7,22 +7,26 @@ import ROOT
 
 def rooeventselector_select(source_file,source_path,dest_file,dest_path,opt_dict):
     """Create a copy of the oldtree selecting events"""
-
+   
     chg_dir(source_file,source_path[:-1])
     oldtree = ROOT.gDirectory.Get(source_path[-1])
     nentries = oldtree.GetEntries()
     newtree = oldtree.CloneTree(0)
     
-    if opt_dict["first"] != None: first_event = opt_dict["first"]
-    else: first_event = 0
-    if opt_dict["last"] != None: last_event = opt_dict["last"]
-    else: last_event = nentries - 1
-
+    if opt_dict["first"] != None:
+        first_event = opt_dict["first"]
+    else:
+        first_event = 0
+    if opt_dict["last"] != None:
+        last_event = opt_dict["last"]
+    else:
+        last_event = nentries - 1
+            
     for i in range(nentries):
         if i<500 and (i >= first_event and i <= last_event): # i<500 for debug
             #print str(i)+" ", for debug
             oldtree.GetEntry(i)
             newtree.Fill()
-
+                
     chg_dir(dest_file,dest_path)
     newtree.Write()
